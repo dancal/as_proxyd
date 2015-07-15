98d4270e771f22bc380e5c1f97528ef0d356b1bc
dancal <dancal@widerplanet.com>
2015-07-15 14:15:33 +0900

    modified:   include/config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -1,6 +1,6 @@
 const char SERVER_VER[] = {  "0.7.1 (Build : " __DATE__ " " __TIME__ ")"};
 
 #define CONFIG_FILE 		"conf/config.ini"
-#define CONFIG_LOG_FILE     "/var/log/bid_engine.log"
+#define CONFIG_LOG_FILE     "/var/log/as_proxyd.log"
 
 #define AS_HEALTH_TIMER		1 * 1000	
